2018140780cbaca6097e72e71375d382894e31e0
Ricky Joudrey <rjoudrey@users.noreply.github.com>
2016-10-11 21:47:09 -0400

    Initial Commit

---
+++ multiple-cursors/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  multiple-cursors
+//
+//  Created by Ricky Joudrey on 10/11/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ multiple-cursors/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  multiple-cursors
+//
+//  Created by Ricky Joudrey on 10/11/16.
+//  Copyright © 2016 com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
